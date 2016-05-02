c55ef8bdacc7f7ebf9f61a38d4e0cdc7a9d3d680
Stanislav Kabin <me@h-zone.ru>
2016-05-02 13:38:40 +0300

    Console commit

---
+++ config/vkgc.php
@@ -0,0 +1,70 @@
+<?php
+return [
+	'languages' => [
+		0  => [
+			'code'  => 'ru',
+			'title' => 'Русский',
+		],
+		1  => [
+			'code'  => 'ua',
+			'title' => 'Українська мова',
+		],
+		3  => [
+			'code'  => 'en',
+			'title' => 'English',
+		],
+		4  => [
+			'code'  => 'es',
+			'title' => 'Español',
+		],
+		6  => [
+			'code'  => 'de',
+			'title' => 'Deutsch',
+		],
+		7  => [
+			'code'  => 'it',
+			'title' => 'Italiano',
+		],
+		12 => [
+			'code'  => 'pt',
+			'title' => 'Portoghese',
+		],
+		16 => [
+			'code'  => 'fr',
+			'title' => 'Français',
+		],
+	],
+	'country'   => [
+		'url'    => 'http://api.vk.com/method/database.getCountries?',
+		'params' => [
+			'v'        => '5.5',
+			'need_all' => 1,
+			'count'    => 1000,
+			'lang'     => null,
+			'offset'   => 0,
+		],
+	],
+	'region'    => [
+		'url'    => 'http://api.vk.com/method/database.getRegions?',
+		'params' => [
+			'v'          => '5.5',
+			'need_all'   => 1,
+			'count'      => 1000,
+			'lang'       => null,
+			'offset'     => 0,
+			'country_id' => null,
+		],
+	],
+	'city'      => [
+		'url'    => 'http://api.vk.com/method/database.getCities?',
+		'params' => [
+			'v'          => '5.5',
+			'need_all'   => 1,
+			'count'      => 100,
+			'lang'       => null,
+			'offset'     => 0,
+			'country_id' => null,
+			'region_id'  => null,
+		],
+	],
+];
